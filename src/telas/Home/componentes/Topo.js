f7af8d689962255a69cf841004c3ac96fe4d64dc
Guilherme Vieira <vieiragui21@gmail.com>
2022-11-09 10:38:42 -0300

    Consumindo dados na tela Topo

--- src/telas/Home/componentes/Topo.js
+++ src/telas/Home/componentes/Topo.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import { Image, StyleSheet, Text, View } from 'react-native'
 
+import { carregaTopo } from '../../../servicos/carregaDados' //como não estou exportando como default preciso usar chaves
 import logo from '../../../assets/logo.png'
 
-export default function Topo() {
-  return (
-    <View style={estilos.topo}>
-      <Image style={estilos.imagem} source={logo} />
-      <Text style={estilos.boasVindas}>Olá Guilherme</Text>
-      <Text style={estilos.legenda}>Encontre os melhores produtores</Text>
-    </View>
-  )
+class Topo extends React.Component {
+  atualizaTopo() {
+    const retorno = carregaTopo()
+  }
+
+  componentDidMount() {
+    this.atualizaTopo()
+  }
+
+  render() {
+    return (
+      <View style={estilos.topo}>
+        <Image style={estilos.imagem} source={logo} />
+        <Text style={estilos.boasVindas}>Olá Guilherme</Text>
+        <Text style={estilos.legenda}>Encontre os melhores produtores</Text>
+      </View>
+    )
+  }
 }
 
 const estilos = StyleSheet.create({
@@ -33,3 +44,5 @@ const estilos = StyleSheet.create({
     lineHeight: 26
   }
 })
+
+export default Topo
